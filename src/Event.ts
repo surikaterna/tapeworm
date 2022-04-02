b32880afe40eccbf33a18c9a606359af0736f732
Kenny Ek <kenny.ek@surikat.se>
2022-04-02 21:31:41 +0200

    Convert Event to TS

---
+++ src/Event.ts
@@ -0,0 +1,20 @@
+export interface Event<Data extends object = Record<string, unknown>, Metadata extends object = Record<string, unknown>> {
+  id: string;
+  type: string;
+  data: Data;
+  timestamp: Date;
+  metadata: Metadata;
+  revision: string | null;
+}
+
+export class Event<Data extends object = Record<string, unknown>, Metadata extends object = Record<string, unknown>> {
+  constructor(id: string, type: string, data: Data, metadata?: Metadata) {
+    this.id = id;
+    this.type = type;
+    this.data = data;
+    this.timestamp = new Date();
+    this.metadata = metadata ?? {} as Metadata;
+    // Filled by EventStore on append
+    this.revision = null;
+  }
+}
