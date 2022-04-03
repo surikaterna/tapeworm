c4ab12c83956e3b80dbebe04d0a0e1cc9c1015ca
Kenny Ek <kenny.ek@surikat.se>
2022-04-03 17:57:05 +0200

    Convert InMemoryPersistenceStore to TS

--- src/persistence/inmemory/InMemoryPersistenceStore.ts
+++ src/persistence/inmemory/InMemoryPersistenceStore.ts
@@ -1,20 +1,23 @@
 import Promise from 'bluebird';
+import { Callback } from '../../types';
 import {InMemoryPartition as Partition} from './InMemoryPartition';
 
-export class InMemoryPersistenceStore {
+export class InMemoryPersistenceStore<T extends object> {
+  _partitions: Record<string, Partition<T>>;
+
   constructor() {
-    this._partitions = [];
+    this._partitions = {};
   }
 
-  _promisify(value, callback) {
+  _promisify<Value>(value: Value, callback?: Callback<Value>) {
     return Promise.resolve(value).nodeify(callback);
   }
 
-  openPartition(partitionId, callback) {
+  openPartition(partitionId: string, callback?: Callback<Partition<T>>) {
     return this._promisify(this._getPartition(partitionId), callback);
   }
 
-  _getPartition(partitionId) {
+  _getPartition(partitionId?: string) {
     const currentPartitionId = partitionId || 'master';
     let partition = this._partitions[currentPartitionId];
     if (partition == null) {
